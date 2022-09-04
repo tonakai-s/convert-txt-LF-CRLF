8ffc22a075a03d29b784f5d9460787255d494f33
suicideDuck22 <rdorneles64@gmail.com>
2022-09-04 12:36:16 -0300

    Added style to the form

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import bodyParser from "body-parser"
 import cors from "cors"
 import Express from "express"
 
+import { fileURLToPath } from 'url'
+import * as path from 'path'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 import filesRouter from "./routes/files.js"
 import homeRouter from "./routes/sendFilesForm.js"
 
@@ -12,6 +17,8 @@ APP.use(bodyParser.urlencoded({
     extended: true
 }))
 APP.use(cors())
+
+APP.use(Express.static(path.join(__dirname, 'public')))
 
 APP.use("/files", filesRouter)
 APP.use(homeRouter)
